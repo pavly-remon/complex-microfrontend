0d5cc23a6cf31f1884e4d5348d8089952af057ae
Pavly Ayoub <pavly.ayoub@vodafone.com>
2025-09-18 14:24:57 +0300

    create pipeline

--- apps/marketing/webpack/webpack.prod.js
+++ apps/marketing/webpack/webpack.prod.js
@@ -1,11 +1,20 @@
 const { merge } = require("webpack-merge");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
 const commonConfig = require("./webpack.common");
+const packageJson = require("../package.json");
 const prodConfig = {
   mode: "production",
+  output: {
+    filename: "[name].[contenthash].js",
+  },
   plugins: [
-    new HtmlWebpackPlugin({
-      template: "./index.html",
+    new ModuleFederationPlugin({
+      name: "marketing",
+      filename: "remoteEntry.js",
+      exposes: {
+        "./mountApp": "./src/bootstrap",
+      },
+      shared: packageJson.dependencies,
     }),
   ],
 };
