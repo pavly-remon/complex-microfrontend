3e5b714dfcd8cd7f3c0264fd6783e8777f69b8d2
Pavly Ayoub <pavly.ayoub@vodafone.com>
2025-09-18 16:40:05 +0300

    fix: update publicPath in production config

--- apps/container/webpack/webpack.prod.js
+++ apps/container/webpack/webpack.prod.js
@@ -6,6 +6,7 @@ const prodConfig = {
   mode: "production",
   output: {
     filename: "[name].[contenthash].js",
+    publicPath: "/container/latest/",
   },
   plugins: [
     new ModuleFederationPlugin({
