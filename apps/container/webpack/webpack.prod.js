4fd0765da79efe72dcb02b49e8d53667d4d9b124
Pavly Ayoub <pavly.ayoub@vodafone.com>
2025-09-18 17:28:18 +0300

    fix: update marketing remote URL to use HTTPS

--- apps/container/webpack/webpack.prod.js
+++ apps/container/webpack/webpack.prod.js
@@ -12,7 +12,7 @@ const prodConfig = {
     new ModuleFederationPlugin({
       name: "container",
       remotes: {
-        marketing: `marketing@${domain}/marketing/remoteEntry.js`,
+        marketing: `marketing@https://${domain}/marketing/remoteEntry.js`,
       },
     }),
   ],
