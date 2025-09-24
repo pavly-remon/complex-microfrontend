86cf2a775a7015729cdc7f53558ef548acb4b7ee
Pavly Ayoub <pavly.ayoub@vodafone.com>
2025-09-24 15:37:39 +0300

    add nav bar

--- apps/container/src/bootstrap.js
+++ apps/container/src/bootstrap.js
@@ -1,3 +1,17 @@
 import { mount } from "marketing/mountApp";
+import "./index.css";
 
+const container = document.querySelector("#container");
+if (container) {
+  const appTitle = document.createElement("a");
+  appTitle.classList.add("app-title");
+  appTitle.href = "/";
+  appTitle.innerText = "App";
+  container.appendChild(appTitle);
+  const loginLink = document.createElement("a");
+  loginLink.classList.add("login-link");
+  loginLink.href = "/login";
+  loginLink.innerText = "Login";
+  container.appendChild(loginLink);
+}
 mount(document.querySelector("#root"));
--- apps/container/webpack/webpack.common.js
+++ apps/container/webpack/webpack.common.js
@@ -3,6 +3,10 @@ module.exports = {
   module: {
     rules: [
       {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
         test: /\.m?js$/,
         exclude: /node_modules/,
         use: {
